2c77ee7526178677ad8fc21ef3106efc70673b7e
jubEfrei <julien.casamian@efrei.net>
2024-11-12 18:35:55 +0100

    update

--- tree.c
+++ tree.c
@@ -6,11 +6,13 @@
 #include <assert.h>
 #include "tree.h"
 
-t_node *createNode(int value, int depth, int *availablemvt)
+t_node *createNode(int value, int depth, int *availablemvt, t_node *parent)
 {
     t_node *node = (t_node *)malloc(sizeof(t_node));
     node->value = value;
     node->depth = depth;
     node->availablemvt = availablemvt;
-
+    node->nbSons = 9 - depth;
+    node->parent = parent;
+    return node;
 }
--- tree.h
+++ tree.h
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 
 typedef struct s_node {
+    struct s_node *parent;
     int value;
     int depth;
     int nbSons;
