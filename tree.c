8c06be5cec1955fee4c15f7b11f6850f49dc79f7
jubEfrei <julien.casamian@efrei.net>
2024-11-12 18:27:49 +0100

    update

--- tree.c
+++ tree.c
@@ -1,3 +1,16 @@
 //
 // Created by jubos on 12/11/2024.
 //
+
+#include <malloc.h>
+#include <assert.h>
+#include "tree.h"
+
+t_node *createNode(int value, int depth, int *availablemvt)
+{
+    t_node *node = (t_node *)malloc(sizeof(t_node));
+    node->value = value;
+    node->depth = depth;
+    node->availablemvt = availablemvt;
+
+}
--- tree.h
+++ tree.h
@@ -4,5 +4,16 @@
 
 #ifndef TREE_H
 #define TREE_H
+#include <stdlib.h>
+
+typedef struct s_node {
+    int value;
+    int depth;
+    int nbSons;
+    struct s_node **sons;
+    int *availablemvt;
+} t_node;
+
+t_node *createNode(int value, int depth, int *availablemvt);
 
 #endif //TREE_H
